6c428f6a285f6ccc4358ee4771b51e7d80e9c0cc
John Chien <j.ohn10285@gmail.com>
2023-07-31 13:52:44 +0800

    test

---
+++ disable.py
@@ -0,0 +1,19 @@
+import subprocess
+from openpyxl import load_workbook
+
+def execute_shell_commands(commands):
+    for command in commands:
+        subprocess.call(command, shell=True)
+
+def get_excel_column(file_path, column):
+    workbook = load_workbook(file_path)
+    sheet = workbook.active
+    column_values = [cell.value for cell in sheet[column]]
+    return column_values[1:]  # Exclude the header
+
+if __name__ == '__main__':
+    excel_file = 'sdfxcvxc.xlsx'
+    column_letter = 'A'
+    
+    commands = get_excel_column(excel_file, column_letter)
+    execute_shell_commands(commands)
