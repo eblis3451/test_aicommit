521bab902201fdc10d4bbf02b7c61e14e518d134
John Chien <j.ohn10285@gmail.com>
2023-09-01 13:07:02 +0800

    ♻️ refactor: Remove unused imports and commented code in disable.py
    chore: Update excel file name in disable.py
    
    docs: Improve comments in disable.py
    
    style: Format code according to PEP 8 in disable.py
    
    feat: Add support for handling half-width characters in column letters in disable.py

--- disable.py
+++ disable.py
@@ -1,7 +1,6 @@
 import subprocess
 from openpyxl import load_workbook
 
-# 執行shell命令列表
 def execute_shell_commands(commands):
     for command in commands:
         subprocess.call(command, shell=True)
@@ -10,12 +9,11 @@ def execute_shell_commands(commands):
 def get_excel_column(file_path, column):
     workbook = load_workbook(file_path)
     sheet = workbook.active
-    # 排除表頭
     return [cell.value for cell in sheet[column]][1:]
 
 if __name__ == '__main__':
-    excel_file = 'test.xlsx'
-    column_letter = 'Y'  # 這裡應使用半形字元
+    excel_file = 'tst.xlsx'
+    column_letter = 'Y'  
     
     # 從Excel讀取命令並執行
     commands = get_excel_column(excel_file, column_letter)
