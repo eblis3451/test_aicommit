b584fcb62f977f32c0216f2dbf570abd6ddf3c12
John Chien <j.ohn10285@gmail.com>
2023-09-01 13:01:35 +0800

    change file name

--- disable.py
+++ disable.py
@@ -1,19 +1,22 @@
 import subprocess
 from openpyxl import load_workbook
 
+# 執行shell命令列表
 def execute_shell_commands(commands):
     for command in commands:
         subprocess.call(command, shell=True)
 
+# 從Excel的某一列讀取數據
 def get_excel_column(file_path, column):
     workbook = load_workbook(file_path)
     sheet = workbook.active
-    column_values = [cell.value for cell in sheet[column]]
-    return column_values[1:]  # Exclude the header
+    # 排除表頭
+    return [cell.value for cell in sheet[column]][1:]
 
 if __name__ == '__main__':
-    excel_file = 'fdgjl.xlsx'
-    column_letter = 'Ｙ'
+    excel_file = 'test.xlsx'
+    column_letter = 'Y'  # 這裡應使用半形字元
     
+    # 從Excel讀取命令並執行
     commands = get_excel_column(excel_file, column_letter)
     execute_shell_commands(commands)
